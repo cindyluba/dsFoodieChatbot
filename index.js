e6a6cb878ad59baee2303ba832ff11a9d536b8bd
Cindy Lu <cindylulu@ResNet-65-95.resnet.ucsb.edu>
2017-05-27 15:04:07 -0700

    hello world 3

--- index.js
+++ index.js
@@ -30,3 +30,43 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAABsOHkUKx0BALjt7oTRP6sCO3x0K1AkeTLstikZA8goiQZCTYg5GR7wpGm22PwBepAoy420c6cA18XnNZAyFoNZAGW47FtQA3e0o2RZARUpBvznZCZBQUraY6IeUR3Y3lRBKhCkKg00bqWUAQQfJAnZCMBqhxnDSYPR6apXEZCpbRwZDZD"
+
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+
+
